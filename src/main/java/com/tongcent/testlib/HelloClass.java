8e297399766fe6f9b8c86cd893ceaad5de7398d5
superkezhanwang <1143249327@qq.com>
2016-09-20 20:50:44 +0800

    Hello World
    lei

---
+++ src/main/java/com/tongcent/testlib/HelloClass.java
@@ -0,0 +1,11 @@
+package com.tongcent.testlib;
+
+/**
+ * Created by ningdan on 2016/9/20.
+ */
+public class HelloClass {
+    public void sayHello(){
+        System.out.println("Hello world");
+    }
+
+}
